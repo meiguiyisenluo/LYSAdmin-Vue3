a4a03db8b57b4e7a330e54dc42761661685b8892
lys <1402175410@qq.com>
2025-08-17 22:04:57 +0800

    upload

---
+++ src/components/LYSUploader.vue
@@ -0,0 +1,61 @@
+<template>
+  <el-upload
+    class="avatar-uploader"
+    action="/api/upload/cloudflare-r2"
+    :show-file-list="false"
+    :on-success="handleAvatarSuccess"
+    :headers="{
+      'Lys-Token': token,
+    }"
+    method="put"
+  >
+    <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+    <el-icon v-else class="avatar-uploader-icon"><i-ep-plus /></el-icon>
+  </el-upload>
+</template>
+
+<script lang="ts" setup>
+const emit = defineEmits(['success'])
+import { ref } from 'vue'
+
+import type { UploadProps } from 'element-plus'
+
+const imageUrl = ref('')
+
+const handleAvatarSuccess: UploadProps['onSuccess'] = (response, uploadFile) => {
+  emit('success', response)
+  imageUrl.value = URL.createObjectURL(uploadFile.raw!)
+}
+
+import { useTokenStore } from '@/stores/token'
+const { token } = useTokenStore()
+</script>
+
+<style scoped>
+.avatar-uploader {
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+  &:deep() .el-upload {
+    border: 1px dashed var(--el-border-color);
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+    transition: var(--el-transition-duration-fast);
+    &:hover {
+      border-color: var(--el-color-primary);
+    }
+  }
+
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    text-align: center;
+  }
+}
+</style>
--- src/views/Home/IndexView.vue
+++ src/views/Home/IndexView.vue
@@ -1,6 +1,7 @@
 <template>
   <div>{{ user.nickname || user.username }}</div>
   <ElButton type="primary" @click="logout">logout</ElButton>
+  <LYSUploader @success="openImage" />
 </template>
 
 <script lang="ts" setup>
@@ -8,12 +9,15 @@ import { useUserStore } from '@/stores/user'
 const { user } = useUserStore()
 import useLogin from '@/hooks/useLogin'
 const { logout } = useLogin()
+import LYSUploader from '@/components/LYSUploader.vue'
 
-// import { getUserList } from '@/api/user'
+import { getUserList } from '@/api/user'
 
-// getUserList().then(({ data }) => {
-//   console.log(data)
-// })
+getUserList().then(({ data }) => {
+  console.log(data)
+})
+
+const openImage = (res: { url: string }) => window.open(res.url)
 </script>
 
 <style scoped></style>
