762d27539465b52a973097f9225ff8c3aa23f79c
lys <1402175410@qq.com>
2025-09-06 17:46:20 +0800

    heartbeat

--- src/main.ts
+++ src/main.ts
@@ -16,13 +16,13 @@ import { i18n } from '@/stores/i18n'
 
 const app = createApp(App)
 
-const pinia = createPinia()
-pinia.use(piniaPluginPersistedstate)
-app.use(pinia)
-app.use(router)
-app.use(i18n)
-
 import LYSPage from '@/components/layout/LYSPage.vue'
 app.component(LYSPage.name!, LYSPage)
 
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia).use(router).use(i18n)
+
 app.mount('#app')
+
+import('@/api/login.ts').then(({ heartbeat }) => heartbeat())
